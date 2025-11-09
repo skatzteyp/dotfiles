d17df5e04b02dfdb449a7b0301bb659f82f77da6
Sigmund Burgos <sigmund.burgos@gmail.com>
2025-11-10 01:26:33 +0800

    Fix theme not loading

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,13 @@
-require("config.main")
+-- Set leader early (safe even if duplicated in main)
+vim.g.mapleader = "\\"
+
+-- 1) Load plugins (bootstraps lazy.nvim + installs/loads onedark)
 require("config.plugins")
+
+-- 2) Core options, UI, keymaps, colorscheme, etc.
+require("config.main")
+
+-- 3) Plugin-specific configs
 require("config.coc")
 require("config.fzf")
 require("config.gitgutter")
